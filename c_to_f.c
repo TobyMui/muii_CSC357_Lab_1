655f57250faeb1a3b3129570925248f9311b0060
TobyMui <tcmui@calpoly.edu>
2023-10-02 22:17:16 -0700

    initial

--- c_to_f.c
+++ c_to_f.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
+float tempConvert(float celsius){
+	float res; 
+	res = celsius * 1.8 + 32; 
+	return res;
+}
+
+char* analysis(float temp){
+	if(temp >= 100){
+		return "Warning: Heat Wave!";
+	}else if( temp >= 80){
+		return "Hot";
+	}else if( temp >= 41){
+		return "Normal";
+	}else if( temp >= 10){
+		return "Cold";
+	}else{
+		return "Very Cold!";
+	}
+}
+
+
 int main(void)
 {
-	printf("Hello World!\n");
+	float tempC;
+	float tempF; 
+	printf("Please input a temperature in Celsius:");
+	scanf("%f", &tempC);
+	tempF = tempConvert(tempC);
+
+	char *report = analysis(tempF);
+
+	printf("%s \n", report);
+
 }
