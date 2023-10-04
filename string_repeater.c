314be4ad1148422216a54262cc8c4ce84d20e60e
TobyMui <tcmui@calpoly.edu>
2023-10-04 13:53:49 -0700

    string repeater

--- string_repeater.c
+++ string_repeater.c
@@ -1,34 +1,70 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 
 
-void valueChecker(int value){ //verifies that the value is positive 
+void valueChecker(long value){ //verifies that the value is positive 
     if(value<0){
         printf("ERROR!: Value must be greater than 0 \n");
         exit(0);
     }
 }
 
-int main(void){
-    int repVal; 
-    char word[20]; 
+// void wordChecker(char *word){ //checks if there is a \n
+// int i = 0;
+//     while(word[i] != '\0' && word[i] != '\n'){
+//         printf("%c", word[i]);
+//         i++;
+//     }
+//     printf("\n");
+//     // return 0; 
+// }
 
-    printf("Enter the value of repetition: ");
-    scanf("%d", &repVal);
-
-    valueChecker(repVal);
-    
 
 
-    printf("Enter word to be repeated: ");
-    scanf("%s", word);
-    
-    for(int i = 0 ; i < repVal ; i++ ){
-        printf("%s\n", word);
+
+int main(int argc, char *argv[]){
+
+    //Converting string to int 
+    char *s = argv[1];
+    char *endptr; 
+    long num; 
+    num = strtol(s,&endptr,10);
+
+    valueChecker(num); 
+
+    printf( "Number of Repititions: %ld \n", num);
+    printf( "Word to be printed: %s \n" , argv[2]);
+
+    for(int i = 0 ; i < num ; i++ ){
+        printf("%s\n", argv[2]);
     }
 
-
-
+    for(int i = 1; i < argc; i++){
+        printf("argv[%d]  =  %s \n", i , argv[i]);
+    }
+    
+    // wordChecker(argv[2]);
 }
+
+
+
+
+// int main(void){
+//     int repVal; 
+//     char word[20]; 
+
+//     printf("Enter the value of repetition: ");
+//     scanf("%d", &repVal);
+
+//     valueChecker(repVal);
+    
+
+
+//     printf("Enter word to be repeated: ");
+//     scanf("%s", word);
+    
+//     for(int i = 0 ; i < repVal ; i++ ){
+//         printf("%s\n", word);
+//     }
+// }
 
