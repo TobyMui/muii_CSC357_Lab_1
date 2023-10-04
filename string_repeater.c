6283ff260a9620cc6673b251b47b9652533de20d
TobyMui <tcmui@calpoly.edu>
2023-10-03 23:11:44 -0700

    string_repeater initial

--- string_repeater.c
+++ string_repeater.c
@@ -1,2 +1,34 @@
-Hello
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+void valueChecker(int value){ //verifies that the value is positive 
+    if(value<0){
+        printf("ERROR!: Value must be greater than 0 \n");
+        exit(0);
+    }
+}
+
+int main(void){
+    int repVal; 
+    char word[20]; 
+
+    printf("Enter the value of repetition: ");
+    scanf("%d", &repVal);
+
+    valueChecker(repVal);
+    
+
+
+    printf("Enter word to be repeated: ");
+    scanf("%s", word);
+    
+    for(int i = 0 ; i < repVal ; i++ ){
+        printf("%s\n", word);
+    }
+
+
+
+}
 
